b0a541abea7389f8bcdcee46735a8b11192a12ce
Konstantin Berdnikov <constant2play@gmail.com>
2020-10-31 16:45:24 +0300

    config production env
    config production env
    
    config production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.url = 'wss://80.249.146.112:4001/cable'
+  config.action_cable.allowed_request_origins = ['*']
+  # config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
