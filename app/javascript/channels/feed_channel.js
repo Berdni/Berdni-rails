4240bb206226227a09cfe9b213791d7d6d81ff8c
Konstantin Berdnikov <constant2play@gmail.com>
2020-10-31 17:11:12 +0300

    Config redis

--- app/javascript/channels/feed_channel.js
+++ app/javascript/channels/feed_channel.js
@@ -2,10 +2,6 @@ import CableReady from 'cable_ready'
 import consumer from "./consumer"
 
 consumer.subscriptions.create("FeedChannel", {
-  connected() {
-    console.log('Test')
-  },
-
   received(data) {
     if (data.cableReady) CableReady.perform(data.operations)
   }
