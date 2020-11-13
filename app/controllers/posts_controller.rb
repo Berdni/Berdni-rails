3fe7e481198e0b9862e61c79bff9cfb96ba73137
Berdnikov <constant2play@gmail.com>
2020-11-13 19:12:05 +0300

    Add posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  include CableReady::Broadcaster
+  load_and_authorize_resource
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      cable_ready['feed'].insert_adjacent_html(
+        selector: '#pinned-post',
+        position: 'afterend',
+        html: render_to_string(partial: 'post', locals: { post: @post })
+      )
+      cable_ready.broadcast
+      redirect_to root_path
+    else
+      # TODO Flash messages
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    cable_ready['feed'].remove(
+      selector: '#post_' + @post.id.to_s
+    )
+    cable_ready.broadcast
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'posts#index'
+  resources :posts, only: %i[index create destroy]
 
   get '/auth/:provider/callback', to: 'sessions#create', as: :signin
   delete '/logout', to: 'sessions#destroy', as: :logout
