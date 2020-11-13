b48dee819bf369325cdf5083f7d914a0ee185e1f
Konstantin Berdnikov <constant2play@gmail.com>
2020-11-13 09:56:46 +0300

    Add tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  include CableReady::Broadcaster
   load_and_authorize_resource
 
   def index
@@ -11,25 +10,16 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
-      cable_ready['feed'].insert_adjacent_html(
-        selector: '#pinned-post',
-        position: 'afterend',
-        html: render_to_string(partial: 'post', locals: { post: @post, editable: false })
-      )
-      cable_ready.broadcast
       redirect_to root_path
     else
-      # TODO Flash messages
+      redirect_to root_path, notice: 'Невозможно добавить сообщение'
     end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    cable_ready['feed'].remove(
-      selector: '#post_' + @post.id.to_s
-    )
-    cable_ready.broadcast
+    redirect_to root_path
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,24 @@
 class Post < ApplicationRecord
+  include CableReady::Broadcaster
+
   belongs_to :user
   before_save { body.squeeze! }
   validates :body, presence: true, length: { maximum: 200 }
+
+  # TODO Transfer code from Model to ActiveJob
+  after_create do
+    cable_ready['feed'].insert_adjacent_html(
+      selector: '#pinned-post',
+      position: 'afterend',
+      html: ApplicationController.render(partial: 'posts/post', locals: { post: self, editable: false })
+    )
+    cable_ready.broadcast
+  end
+
+  after_destroy do
+    cable_ready['feed'].remove(
+      selector: '#post_' + id.to_s
+    )
+    cable_ready.broadcast
+  end
 end
