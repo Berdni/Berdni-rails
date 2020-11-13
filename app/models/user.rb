2785f3dfe99763bb58fa9f25e1d1645d1b567850
Berdnikov <constant2play@gmail.com>
2020-11-13 19:07:37 +0300

    Add relation with Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
+  has_many :post, dependent: :destroy
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
